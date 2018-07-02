83b26dafae37821ca4a59634618a4e500d42343d
jerrylai <jerrylai@aliyun.com>
2018-07-02 10:49:42 +0800

    添加.netcore2.1 nuget包

---
+++ NETCore2.1/Afx.Data.Entity/EntityLog/EntityLoggerFactory.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Afx.Data.Entity.EntityLog
+{
+    /// <summary>
+    /// Entity LoggerFactory
+    /// </summary>
+    public class EntityLoggerFactory : ILoggerFactory
+    {
+        private ILoggerProvider provider;
+
+        /// <summary>
+        /// Entity LoggerFactory
+        /// </summary>
+        /// <param name="provider">Entity LoggerProvider</param>
+        public void AddProvider(ILoggerProvider provider)
+        {
+            if (provider == null) throw new ArgumentNullException("provider");
+            this.provider = provider;
+        }
+        /// <summary>
+        /// Creates a new Microsoft.Extensions.Logging.ILogger instance.
+        /// </summary>
+        /// <param name="categoryName">The category name for messages produced by the logger.</param>
+        /// <returns>Entity LoggerFactory</returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            return this.provider.CreateLogger(categoryName);
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            if (this.provider != null)
+            {
+                this.provider.Dispose();
+                this.provider = null;
+            }
+        }
+    }
+}
---
+++ NETCore2.1/Afx.Data.Entity/EntityLog/EntityLoggerProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Logging;
+
+namespace Afx.Data.Entity.EntityLog
+{
+    /// <summary>
+    /// Entity LoggerProvider
+    /// </summary>
+    public class EntityLoggerProvider : ILoggerProvider
+    {
+        private EntityContext context;
+        /// <summary>
+        /// Entity LoggerProvider
+        /// </summary>
+        /// <param name="context">EntityContext</param>
+        public EntityLoggerProvider(EntityContext context)
+        {
+            if (context == null) throw new ArgumentNullException("context");
+            this.context = context;
+        }
+
+        /// <summary>
+        /// CreateLogger
+        /// </summary>
+        /// <param name="categoryName">categoryName</param>
+        /// <returns></returns>
+        public ILogger CreateLogger(string categoryName)
+        {
+            if(this.context != null)
+            {
+                return new EntityLogger(this.context, categoryName);
+            }
+
+            return null;
+        }
+
+        /// <summary>
+        /// 释放资源
+        /// </summary>
+        public void Dispose()
+        {
+            this.context = null;
+        }
+    }
+}
