2f7516771efcdbac4c138f0555f3f4c51005550c
jerrylai <jerrylai@aliyun.com>
2018-07-04 10:35:46 +0800

    添加删除pdb

--- build.bat
+++ build.bat
@@ -2,5 +2,7 @@
 "%SYSTEMDRIVE%\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NET20\Afx.NET20.sln" /t:Rebuild /p:Configuration=Release
 "%SYSTEMDRIVE%\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NET40\Afx.NET40.sln" /t:Rebuild /p:Configuration=Release
 "%SYSTEMDRIVE%\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe" "NET452\Afx.NET452.sln" /t:Rebuild /p:Configuration=Release
-dotnet build --configuration Release "NETCore2.1\Afx.NETCore2.1.sln"
-dotnet build --configuration Release "NETStandard2.0\Afx.NETStandard2.0.sln"
+dotnet build "NETCore2.1\Afx.NETCore2.1.sln" -c Release 
+dotnet build "NETStandard2.0\Afx.NETStandard2.0.sln" -c Release 
+cd Publish
+del /q/s *.pdb
