157cf5e3d4b082359a7e65aad42b1a3cdca99cfa
jerrylai <jerrylai@aliyun.com>
2018-07-17 18:37:33 +0800

    优化编译脚本

--- build.bat
+++ build.bat
@@ -1,5 +1,6 @@
 @echo off
 set Build="%SYSTEMDRIVE%\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
+if exist publish rd /s /q publish
 %Build% "NET20\Afx.NET20.sln" /t:Rebuild /p:Configuration=Release
 %Build% "NET40\Afx.NET40.sln" /t:Rebuild /p:Configuration=Release
 %Build% "NET452\Afx.NET452.sln" /t:Rebuild /p:Configuration=Release
--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("6.2.8.0")]
-[assembly: AssemblyFileVersion("6.2.8.0")]
+[assembly: AssemblyVersion("6.2.9.0")]
+[assembly: AssemblyFileVersion("6.2.9.0")]
