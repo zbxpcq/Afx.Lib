c177f9faca65053ca7724cee701ed03f4f97949f
jerrylai <jerrylai@aliyun.com>
2019-03-19 16:28:12 +0800

    移除dotnetcore2.1

--- src/Assembly.cs
+++ src/Assembly.cs
@@ -6,5 +6,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © mycsv 2018")]
 [assembly: AssemblyTrademark("jerrylai@aliyun.com")]
 
-[assembly: AssemblyVersion("8.0.3.0")]
-[assembly: AssemblyFileVersion("8.0.3.0")]
+[assembly: AssemblyVersion("8.2.2.3")]
+[assembly: AssemblyFileVersion("8.2.2.3")]
--- version.cmd
+++ version.cmd
@@ -1,3 +1,3 @@
 @echo off
-set v=8.0.3.0
+set v=8.2.2.3
 tool\EditVersion dir="%cd%" v=%v% a="Assembly.cs"
